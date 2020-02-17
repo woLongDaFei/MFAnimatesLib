813b258aa05abf2e4322a93c627da703025f7608
赵永斐 <zhaoyongfei2008@163.com>
2020-02-17 17:58:38 +0800

    修改文件内容

--- AnimateFlie/classes/XGAForgetPassWordVC.h
+++ AnimateFlie/classes/XGAForgetPassWordVC.h
@@ -7,8 +7,7 @@
 //
 
 #import <Foundation/Foundation.h>
-#import "XGAUserDataViewController.h"
 
-@interface XGAForgetPassWordVC : XGAUserDataViewController
+@interface XGAForgetPassWordVC : NSObject
 
 @end
