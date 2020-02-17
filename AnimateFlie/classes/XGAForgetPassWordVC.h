082c14f2c52ee9f80ce47ede3191181ec3f77e3e
赵永斐 <zhaoyongfei2008@163.com>
2020-02-17 17:01:14 +0800

    提交代码内容

---
+++ AnimateFlie/classes/XGAForgetPassWordVC.h
@@ -0,0 +1,14 @@
+//
+//  XGAForgetPassWordVC.h
+//  GcwClient
+//
+//  Created by wenbo. on 15/11/22.
+//  Copyright (c) 2015年 wenbo. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "XGAUserDataViewController.h"
+
+@interface XGAForgetPassWordVC : XGAUserDataViewController
+
+@end
